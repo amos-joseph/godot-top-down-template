02ea3f865952df0481ced0da4b917888f47a24a1
Amos Joseph <amos.joseph@doane.edu>
2024-01-24 09:26:37 -0600

    Tilemaps, walk, and camera working

--- assets/terrain/CharacterBody2D.gd
+++ assets/terrain/CharacterBody2D.gd
@@ -10,6 +10,11 @@ func _ready():
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
+	if velocity.x != 0 || velocity.y != 0:
+		sprite.play("walk")
+	else:
+		sprite.play("idle")
+		
 
 	move_and_slide()
 	# position = Vector2(snapped(position.x, 1), snapped(position.y, 1))
